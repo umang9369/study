f31293ed95e70d386510f684fe8b17ea2b6fbc28
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-13 09:22:23 +0530

    Create combin-sum.cpp

---
+++ combin-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(int i, vector<int>& arr, int target,
+               vector<int>& combin, vector<vector<int>>& ans) {
+        
+        if (target == 0) {
+            ans.push_back(combin);
+            return;
+        }
+
+        if (i == arr.size() || target < 0) {
+            return;
+        }
+
+        // Pick the current element
+        combin.push_back(arr[i]);
+        solve(i, arr, target - arr[i], combin, ans);
+        combin.pop_back();
+
+        // Skip the current element
+        solve(i + 1, arr, target, combin, ans);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        vector<vector<int>> ans;
+        vector<int> combin;
+        solve(0, arr, target, combin, ans);
+        return ans;
+    }
+};
