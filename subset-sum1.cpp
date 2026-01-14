4bac327f72f0144f6010d4b2fe6b4b1cb8a087a1
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-15 04:32:18 +0530

    Create subset-sum1.cpp

---
+++ subset-sum1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    void solve(int i , int current , vector<int>& arr , vector<int>& sums ){
+        //basecase
+        if(i == arr.size()){
+            sums.push_back(current);
+            return ;
+        }
+        //take
+        solve( i+1 , current + arr[i] , arr , sums );
+        //untake
+        solve( i+1 , current , arr , sums );
+        return;
+    }
+    vector<int> subsetSums(vector<int>& arr) {
+        vector<int> sums;
+        solve(0,0,arr,sums);
+        sort ( sums.begin() , sums.end() );
+        return sums ;
+    }
+};
