a69e01f0fa2367a89fb1c89ff8948237a31416b3
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-15 05:20:56 +0530

    Implement Combination Sum III solution

---
+++ Combination-Sum-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(int k, int n, vector<vector<int>>& ans, vector<int> ans2,int sum , int idx){
+        if(sum == n && k == 0){
+            ans.push_back(ans2);
+            return ;
+        }
+        if(sum > n){
+            return ;
+        }
+        for(int i = idx ; i <= 9 ; i++){
+            if(i > n){
+                break ;
+            }
+            ans2.push_back(i);
+            solve( k-1 , n , ans , ans2 , sum+i , i+1 );
+            ans2.pop_back();
+        }
+        return ;
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans ;
+        vector<int> ans2 ;
+        solve( k , n , ans , ans2 , 0 , 1 );
+        return ans;
+    }
+};
