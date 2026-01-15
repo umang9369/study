ed42985781bea0f076acf7f82d98781615d1a5e4
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-15 06:17:52 +0530

    Create phone-number-problem.cpp

---
+++ phone-number-problem.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> result;
+    void solve(int i , string& digits , string& temp , unordered_map <char , string>& mp){
+        if( i>= digits.length()){
+            result.push_back(temp);
+            return;
+        }
+        char ch = digits[i];
+        string str = mp[ch];
+        for(int j=0;j<str.length();j++){
+            temp.push_back(str[j]);
+            solve(i+1,digits,temp,mp);
+            temp.pop_back();
+        }
+    };
+    vector<string> letterCombinations(string digits) { 
+        if(digits.length() == 0 ){
+            return {};
+        }
+        unordered_map <char , string> mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+        string temp = "";
+        solve(0,digits,temp,mp);
+        return result;
+    }
+};
