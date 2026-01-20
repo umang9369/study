a524a523102bf85a2d90b6775fbdcd8b4bc4b4ba
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-20 22:52:51 +0530

    Create N-Queens.cpp

---
+++ N-Queens.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        // Check upper-left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // Check upper-right diagonal
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void nqueen(vector<vector<string>>& ans, vector<string>& board, int row, int n) {
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+                nqueen(ans, board, row + 1, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+
+        nqueen(ans, board, 0, n);
+        return ans;
+    }
+};
