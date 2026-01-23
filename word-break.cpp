fa10f19678de0fdb74019bc1230f10cba9a721ab
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-23 16:02:36 +0530

    Create word-break.cpp

---
+++ word-break.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int n;
+    int dp[301];
+    unordered_set<string> st;
+    bool solve(int idx, string &s) {
+        if (idx >= n) {
+            return true;
+        }
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+        for (int l = 1; l <= n-idx; l++) {   
+            string temp = s.substr(idx, l);
+            if (st.find(temp) != st.end() && solve(idx + l, s)) {
+                return dp[idx] = true;
+            }
+        }
+        return dp[idx] = false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n = s.length();
+        memset(dp, -1, sizeof(dp));
+        for (string &word : wordDict) {
+            st.insert(word);
+        }
+        return solve(0, s); 
+    }
+};
