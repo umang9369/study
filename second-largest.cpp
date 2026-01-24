84503ff5f6723d07ba5f09106515d79917b10640
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-24 22:07:33 +0530

    Create second-largest.cpp

---
+++ second-largest.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &nums) {
+        int largest = nums[0];
+        int slargest = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > largest) {
+                largest = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > slargest && nums[i] != largest) {
+                slargest = nums[i];
+            }
+        }
+        return slargest;
+    }
+};
