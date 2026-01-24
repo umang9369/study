67fab7868b2c7edaf3a30fbd131fe3bc1811c30f
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-24 21:42:38 +0530

    Create largest-array.cpp

---
+++ largest-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int largest(vector<int> &nums) {
+    int largest = nums[0];
+    for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > largest) {
+                largest = nums[i];
+            }
+        }
+        return largest;
+    }
+};
+
