150557f6265b89d2e815ede41592c8a5264ebe1b
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-27 23:46:18 +0530

    Create singleNumber.cpp

---
+++ singleNumber.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int value = 0;
+        for(int i=0;i<nums.size();i++){
+            value = value^nums[i];
+        }
+        return value;
+    }
+};
