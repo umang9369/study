72268492362ee88cfec6b84174cb91fc14eb3b5f
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-29 20:05:28 +0530

    Create 2-sum.cpp

---
+++ 2-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        map <int , int> mp;
+        for(int i=0;i<n;i++){
+            int temp = target - nums[i];
+            if(mp.find(temp) != mp.end()){
+                return {mp[temp],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
