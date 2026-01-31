ae8946f2416b388aa70d1d98b5ae97c266a29fb8
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-01-31 06:10:16 +0530

    Create best-time-to-buy-stock&sell.cpp

---
+++ best-time-to-buy-stock&sell.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxprofit=0;
+        int mincount=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            mincount=min(mincount,prices[i]);
+            maxprofit=max(maxprofit,prices[i]-mincount);
+        }
+        return maxprofit;
+    }
+};
