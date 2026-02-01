b98e19cefb2f6ad2a739788d122359b900eb3c2c
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-02-01 15:01:01 +0530

    Create rearrange-elements-by-sign.cpp

---
+++ rearrange-elements-by-sign.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> result(nums.size());
+        int pi = 0;
+        int ni = 1;
+        for(int &num : nums){
+            if(num > 0){
+                result[pi] = num;
+                pi += 2; 
+            } else {
+                result[ni] = num;
+                ni += 2;
+            }
+        }
+        return result;
+    }
+};
