c91736034ff84a56976105009aca530f877576b5
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-02-09 18:13:21 +0530

    Create Merge-Sorted-Array.cpp

---
+++ Merge-Sorted-Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m+n-1;
+        int j=m-1;
+        int k=n-1;
+        while(k>=0){
+            if(j>=0 && nums1[j] > nums2[k] ){
+                nums1[i]=nums1[j];
+                i--;
+                j--;
+            }else{
+                nums1[i]=nums2[k];
+                i--;
+                k--;
+            }
+        }
+    }
+};
