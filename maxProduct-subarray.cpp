abe5033912946cf6784bf997fc8c1575afd5bbeb
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-02-10 23:36:55 +0530

    Create maxProduct-subarray.cpp

---
+++ maxProduct-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pref=1;
+        int suff=1;
+        int ans=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(pref==0){
+                pref = 1;
+            }
+            if(suff==0){
+                suff = 1;
+            }
+            pref=pref*nums[i];
+            suff=suff*nums[nums.size()-1-i];
+            ans=max(ans,max(pref,suff));
+        }
+        return ans;
+    }
+};
