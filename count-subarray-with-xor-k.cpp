2edb9fb91d607ed7f0764d592716b207d70d4099
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-02-17 15:07:52 +0530

    Create count-subarray-with-xor-k.cpp

---
+++ count-subarray-with-xor-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        int count=0;
+        long xr=0;
+        unordered_map<int,int> fmap;
+        fmap[0] = 1;
+        for(int i=0;i<arr.size();i++){
+            xr ^= arr[i];
+            if (fmap.find(xr^k) != fmap.end()) {
+                count += fmap[xr^k];
+            }
+            fmap[xr]++;
+        }
+        return count;
+    }
+};
