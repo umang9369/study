96041d358403fe6972b859f5b432bd94d790843e
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-02-20 04:33:18 +0530

    Create floor-ciel.cpp

---
+++ floor-ciel.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int findFloor(vector<int>& arr, long long x) {
+        int low=0;
+        int high=arr.size()-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<=x){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
