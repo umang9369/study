0be76f43568015fbac09f0bc522a861c3ec1eb34
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-02-22 23:48:28 +0530

    Create Search-Rotated-Sorted-Array-II.cpp

---
+++ Search-Rotated-Sorted-Array-II.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<high && nums[low]==nums[low+1]){
+            low++;
+        }
+        while(low<high && nums[high]==nums[high-1]){
+            high--;
+        }
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[mid]>nums[high]){
+                if(nums[low]<=target && target<nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(nums[mid]<target && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
