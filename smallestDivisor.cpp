78aa42ab1157eb05e3ec9e1624b39e20ab907c32
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-02-26 23:09:51 +0530

    Implement smallestDivisor function to find divisor

---
+++ smallestDivisor.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int sum(vector<int>& nums, int divisor){
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum += ceil((double)(nums[i])/(double)(divisor));
+        }
+        return sum;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low=1;
+        int high= *max_element(begin(nums),end(nums));
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(sum(nums,mid) <= threshold){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
