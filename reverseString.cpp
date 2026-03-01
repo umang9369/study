f15938dedd2915feb4e441312ec381ebdd9e4877
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-03-02 00:46:41 +0530

    Create reverseString.cpp

---
+++ reverseString.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l=0;
+        int r=s.size()-1;
+        while(l<=r){
+            int temp=0;
+            temp=s[l];
+            s[l]=s[r];
+            s[r]=temp;
+            l++;
+            r--;
+        }
+    }
+};
