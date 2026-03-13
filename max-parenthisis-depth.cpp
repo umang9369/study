307d97257154b72b0e4f9f1b0a6f5dd9eb689158
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-03-13 17:35:17 +0530

    Create max-parenthisis-depth.cpp

---
+++ max-parenthisis-depth.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count=0;
+        int max=INT_MIN;
+        for(auto c : s){
+            if(c=='('){
+                count++;
+            }
+            if(c==')'){
+                count--;
+            }
+            if(count>max){
+                max=count;
+            }
+        }
+        return max;
+    }
+};
