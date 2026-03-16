1328dc7850ffb8cd9d07a869fb14a0df407dc78b
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-03-16 09:04:42 +0530

    Create numberOfSubstrings.cpp

---
+++ numberOfSubstrings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n=s.size();
+        int l=0;
+        int res=0;
+        unordered_map<char,int> mp={{'a',0},{'b',0},{'c',0}};
+        for(int r=0;r<n;r++){
+            mp[s[r]]++;
+            while(mp['a']>0 && mp['b']>0 && mp['c']>0){
+                res += n-r;
+                mp[s[l]]--;
+                l++;
+            }
+        }
+        return res;
+    }
+};
