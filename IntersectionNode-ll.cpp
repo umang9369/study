827e7c4ae404ee6cf39a2ab95115faf83c7c42b0
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-04-10 02:52:36 +0530

    Create IntersectionNode-ll.cpp

---
+++ IntersectionNode-ll.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    int lengtha(ListNode *headA){
+        int l1=0;
+        while(headA != NULL){
+            headA=headA->next;
+            l1++;
+        }
+        return l1;
+    }
+    int lengthb(ListNode *headB){
+        int l2=0;
+        while(headB != NULL){
+            headB=headB->next;
+            l2++;
+        }
+        return l2;
+    }
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lena=lengtha(headA);
+        int lenb=lengthb(headB);
+        while(lena>lenb){
+            lena--;
+            headA=headA->next;
+        }
+        while(lena<lenb){
+            lenb--;
+            headB=headB->next;
+        }
+        while(headA != headB){
+            headA=headA->next;
+            headB=headB->next;
+        }
+        return headA;
+    }
+};
