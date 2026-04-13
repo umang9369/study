73c912064a70888d114d8bdb21daa1f7371a9907
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-04-14 02:27:22 +0530

    Create sor-0,1,2-ll.cpp

---
+++ sor-0,1,2-ll.cpp
@@ -0,0 +1,47 @@
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        if(head==NULL || head->next==NULL) return head;
+        Node* zerohead=new Node(-1);
+        Node* onehead=new Node(-1);
+        Node* twohead=new Node(-1);
+        
+        Node* zero=zerohead;
+        Node* one=onehead;
+        Node* two=twohead;
+        Node* temp=head;
+        while(temp){
+            if(temp->data == 0){
+                zero->next=temp;
+                zero=zero->next;
+            }else if(temp->data==1){
+                one->next=temp;
+                one=one->next;
+            }else if(temp->data==2){
+                two->next=temp;
+                two=two->next;
+            }
+            temp=temp->next;
+        }
+        zero->next=(onehead->next)?onehead->next:twohead->next;
+        one->next=twohead->next;
+        two->next=NULL;
+        Node* newhead=zerohead->next; 
+        delete(zerohead);
+        delete(onehead);
+        delete(twohead);
+        return newhead;
+    }
+};
