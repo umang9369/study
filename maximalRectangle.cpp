7a77dcba5d9f6b60c2e7986fdd20e714915f43be
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-05-16 23:16:17 +0530

    Create maximalRectangle.cpp

---
+++ maximalRectangle.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<int> getls(vector<int>& heights,int n){
+        vector<int> result(n);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                result[i]=-1;
+            }else{
+                while(!st.empty() && heights[st.top()]>=heights[i]){
+                    st.pop();
+                }
+                result[i]=st.empty()?-1:st.top();
+            }
+            st.push(i);
+        }
+        return result;
+    };
+    vector<int> getrs(vector<int>& heights,int n){
+        vector<int> result(n);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+                result[i]=n;
+            }else{
+                while(!st.empty() && heights[st.top()]>heights[i]){
+                st.pop();
+            }
+            result[i]=st.empty()?n:st.top();
+            }
+            st.push(i);
+        }
+        return result;
+    };
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        int maxiarea=0;
+        vector<int> ls =getls(heights,n);
+        vector<int> rs =getrs(heights,n);
+        for(int i=0;i<n;i++){
+            int area=(rs[i]-ls[i]-1)*heights[i];
+            maxiarea=max(maxiarea,area);
+        }
+        return maxiarea;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int maxi=INT_MIN;
+        vector<int>histogram(matrix[0].size(),0);
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<histogram.size();j++){
+                if(matrix[i][j]=='1'){
+                    histogram[j]++;
+                }else{
+                    histogram[j]=0;
+                }
+            }
+            maxi=max(maxi,largestRectangleArea(histogram));
+        }
+        return maxi;
+    }
+};
