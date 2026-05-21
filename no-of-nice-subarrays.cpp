473b2727bff690ebf096a46ae17e77cb15d9abbb
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-05-21 16:48:07 +0530

    Create no-of-nice-subarrays.cpp

---
+++ no-of-nice-subarrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int atmost(vector<int>& nums, int k){
+        if(k<0)return 0;
+        int l=0;
+        int r=0;
+        int count=0;
+        int sum=0;
+        int n=nums.size();
+        while(r<n){
+            sum += nums[r]%2;
+            while(sum>k){
+                sum -=nums[l]%2;
+                l++;
+            }
+            count+=(r-l+1);
+            r++;
+        }
+        return count;
+    }
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atmost(nums,k)-atmost(nums,k-1);
+    }
+};
