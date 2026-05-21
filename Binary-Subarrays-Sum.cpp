97f6ebdab285a4d9b70ed9622c01cf7af3e24e85
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-05-21 16:38:19 +0530

    Create Binary-Subarrays-Sum.cpp

---
+++ Binary-Subarrays-Sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int atmost(vector<int>& nums, int k){
+        if(k<0)return 0;
+        int l=0;
+        int r=0;
+        int count=0;
+        int sum=0;
+        int n=nums.size();
+        while(r<n){
+            sum+=nums[r];
+            while(sum >k){
+                sum -= nums[l];
+                l++;
+            }
+            count+=(r-l+1);
+            r++;
+        }
+        return count;
+    }
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+       return atmost(nums,goal)-atmost(nums,goal-1); 
+    }
+};
