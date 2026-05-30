ccb99eba3fd77fc501eb27fca7038e25c6b7ca0a
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-05-31 02:12:03 +0530

    Create jump-game-2.cpp

---
+++ jump-game-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l=0;
+        int r=0;
+        int jump=0;
+        int n=nums.size();
+        while(r<n-1){
+            int farthest=0;
+            for(int i=l;i<=r;i++){
+                farthest=max(i+nums[i],farthest);
+            }
+            jump++;
+            l=r+1;
+            r=farthest;
+        }
+        return jump;
+    }
+};
