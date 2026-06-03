c405d46282ad34c520110d175c47550c68389b91
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-06-04 03:06:44 +0530

    Create candy-greedy-hard.cpp

---
+++ candy-greedy-hard.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int i=1;
+        int n=ratings.size();
+        int candy=n;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                continue;
+            }
+            int peak=0;
+            while(ratings[i]>ratings[i-1]){
+                peak++;
+                candy+=peak;
+                i++;
+                if(i==n)return candy;
+            }
+            int dip=0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                dip++;
+                candy+=dip;
+                i++;
+            }
+            candy-=min(peak,dip);
+        }
+        return candy;
+    }
+};
