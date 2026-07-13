fef106d3abcb044b56b6f45ab539fdd18b7174fe
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-07-13 16:23:31 +0530

    Create check-valid-bst..cpp

---
+++ check-valid-bst..cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool solve(TreeNode* root,TreeNode* min,TreeNode* max){
+        if(root==NULL)return true;
+        if(min != NULL && root->val <=min->val)return false;
+        if(max != NULL && root->val >=max->val)return false;
+        return solve(root->left,min,root) && solve(root->right,root,max);
+    }
+    bool isValidBST(TreeNode* root) {
+        return solve(root,NULL,NULL);
+    }
+};
