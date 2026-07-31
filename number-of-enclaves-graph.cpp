606c4ca9c8dbbf91b5dc16cc49c505c940551bc5
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-07-31 06:07:53 +0530

    Create number-of-enclaves-graph.cpp

---
+++ number-of-enclaves-graph.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& grid,int u,int v){
+        int m=grid.size();
+        int n=grid[0].size();
+        if(u>=m || u<0 || v>=n || v<0 || grid[u][v]==0)return;
+        grid[u][v]=0;
+        dfs(grid,u+1,v);
+        dfs(grid,u,v+1);
+        dfs(grid,u-1,v);
+        dfs(grid,u,v-1);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==m-1 || j==n-1){
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
