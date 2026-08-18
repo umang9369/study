1f783ff9a1f47a2cbeab6ed52e446b077a043c6a
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-08-19 02:56:02 +0530

    Create networkDelayTime.cpp

---
+++ networkDelayTime.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int, vector<pair<int,int>>>adj;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(auto &vec:times){
+            int u=vec[0];
+            int v=vec[1];
+            int w=vec[2];
+            adj[u].push_back({v,w});
+        }
+        vector<int>res(n+1,INT_MAX);
+        res[k]=0;
+        pq.push({0,k});
+        while(!pq.empty()){
+            int d=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            for(auto &vec:adj[node]){
+                int adjnode=vec.first;
+                int dist=vec.second;
+                if(d+dist<res[adjnode]){
+                    res[adjnode]=d+dist;
+                    pq.push({d+dist,adjnode});
+                }
+            }
+        }
+        int ans=INT_MIN;
+        for(int i=1;i<=n;i++){
+            ans=max(ans,res[i]);
+        }
+        return ans==INT_MAX?-1:ans;
+    }
+};
