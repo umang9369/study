41176e3da9f4a95dcf7ee9a9f5ddd48c1f04c6d0
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-09-03 04:15:50 +0530

    Create find-pivot-index.cpp

---
+++ find-pivot-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        int leftsum=0;
+        for(int i=0;i<nums.size();i++){
+            int rightsum=sum-leftsum-nums[i];
+            if(rightsum==leftsum)return i;
+            leftsum +=nums[i];
+        }
+        return -1;
+    }
+};
