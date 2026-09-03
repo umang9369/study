8cc48aca8c5f10893c3b8928c822544fc78330d4
Umang Singh <141339865+umang9369@users.noreply.github.com>
2026-09-04 02:15:10 +0530

    Create remove-adj-duplicates-string.cpp

---
+++ remove-adj-duplicates-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res;
+        for(auto ch : s){
+            if(!res.empty()&&res.back()==ch){
+                res.pop_back();
+            }else{
+                res.push_back(ch);
+            }
+        }
+        return res; 
+    }
+};
