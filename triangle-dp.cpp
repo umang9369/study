d8761055da6fe519bb5db23fc5045fa515069c48
turnoffthiscomputer <141339865+umang9369@users.noreply.github.com>
2026-09-19 00:09:37 +0530

    Create triangle-dp.cpp

---
+++ triangle-dp.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> t=triangle;
+        for(int row=n-2;row>=0;row--){
+            for(int col=0;col<=row;col++){
+                t[row][col]=t[row][col]+ min(t[row+1][col],t[row+1][col+1]);
+            }
+        }
+        return t[0][0];
+    }
+};
