9a714c8844f540059c668ac0baf98b7f73702746
turnoffthiscomputer <141339865+umang9369@users.noreply.github.com>
2026-09-22 04:27:21 +0530

    Create differenceOfSums.cpp

---
+++ differenceOfSums.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int nums1=0;
+        int nums2=0;
+        for(int i=1;i<=n;i++){
+            if(i%m!=0){
+                nums1+=i;
+            }else{
+                nums2+=i;
+            }
+        }
+        return nums1-nums2;
+    }
+};
